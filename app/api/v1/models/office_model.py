009f3692eb94602f9192497011f6b3eda0f92ffd
softMaina <allancodes@gmail.com>
2019-02-08 00:21:55 +0300

    [Starts #163793698] admin can create a political office

---
+++ app/api/v1/models/office_model.py
@@ -0,0 +1,37 @@
+from flask import Flask, abort
+
+OFFICES = []
+class Office(object):
+    def __init__(self):
+        self.office = OFFICES
+    
+    # create party
+    def add_office(self,name,office_type):
+        office={
+            'id':len(self.office)+1,
+            'name':name,
+            'office_type':office_type
+        }
+        self.office.append(office)
+
+    # def get_offices(self):
+    #     return self.office
+
+    # def update_office(self,office_id,name,office_type):
+        
+    #     office = [task for task in self.office if task['id'] == office_id]
+        
+    #     if len(office) == 0:
+    #         return abort(400)
+    #     office[0]['name'] = name
+    #     office[0]['office_type']= office_type
+        
+
+
+    # def delete_office(self,office_id):
+
+    #     office = [task for task in self.office if task['id'] == office_id]
+
+    #     if len(office) == 0:
+    #         abort(400)
+    #     self.office.remove(office[0])
---
+++ app/api/v1/views/office_views.py
@@ -0,0 +1,62 @@
+
+from flask import Flask, Blueprint, request, make_response, jsonify
+from app.api.v1.models import office_model
+
+OFFICE = office_model.Office()
+
+# return all the political parties
+office_route = Blueprint('office',__name__,url_prefix='/api/v1/office')
+# @office_route.route('',methods=['GET'])
+# def get_offices():
+#     data = OFFICE.get_offices()
+#     return make_response(jsonify({
+#         'status':200,
+#         'data':[{"party":data}]
+#     })),200
+
+@office_route.route('/add',methods=['POST'])
+def add_office():
+  
+    try:
+        data = request.get_json(force=True)
+    except:
+        return make_response(jsonify({
+            "status":400,
+            "message":"wrong input"
+        })),400  
+    name = data["name"]
+    office_type = data["office_type"]
+
+    OFFICE.add_office(name, office_type)
+    return make_response(jsonify({
+        "status":201,
+        "data":[{"message":"success"}]
+    })),201
+
+# @office_route.route('/update/<int:office_id>',methods=['PUT'])
+# def update_office(office_id):
+#     try:
+#         data = request.get_json(force=True)
+#     except:
+#         return make_response(jsonify({
+#             "status":400,
+#             "message":"wrong input"
+#         })),400
+#     id=office_id
+#     name = data["name"]
+#     office_type = data["office_type"]
+  
+
+#     OFFICE.update_office(id,name,office_type)
+#     return make_response(jsonify({
+#         "status":200,
+#         "data":[{"message":"success"}]
+#     })),200
+
+# @office_route.route('/delete/<int:office_id>',methods=['DELETE'])
+# def delete_office(office_id):
+#     OFFICE.delete_office(office_id)
+#     return make_response(jsonify({
+#         "status":200,
+#         "data":[{"message":"sucessful"}]
+#     }))
